91cced0d4cea86b8dc163e510bd6596c5a7c66a1
ricky cancro <ricky@pinterest.com>
2021-07-30 14:29:04 -0700

    type icons

---
+++ Pokedex/Categories/PIPokemonType+TypeIcon.h
@@ -0,0 +1,19 @@
+//
+//  PIPokemonType+TypeIcon.h
+//  Pokedex
+//
+//  Created by ricky cancro on 7/30/21.
+//
+
+#import <UIKit/UIKit.h>
+#import "PIPokemonType.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface PIPokemonType (TypeIcon)
+
+- (UIImage *)imageForType;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Pokedex/Categories/PIPokemonType+TypeIcon.m
@@ -0,0 +1,18 @@
+//
+//  PIPokemonType+TypeIcon.m
+//  Pokedex
+//
+//  Created by ricky cancro on 7/30/21.
+//
+
+#import "PIPokemonType+TypeIcon.h"
+
+@implementation PIPokemonType (TypeIcon)
+
+- (UIImage *)imageForType
+{
+    return nil;
+}
+
+
+@end
