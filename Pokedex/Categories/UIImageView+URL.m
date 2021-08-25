6955b846f6adfec2b6560f126389649b8843f98e
Marcelo Queiroz <marceloqueiroz@pinterest.com>
2021-08-25 15:55:47 -0400

    Fix image loading for cell usage

--- Pokedex/Categories/UIImageView+URL.m
+++ Pokedex/Categories/UIImageView+URL.m
@@ -18,10 +18,14 @@
 {
     self.image = nil;
     self.backgroundColor = placeholderColor;
+    self.tag = [url hash];
     
     NSURLSessionTask *task = [NSURLSession.sharedSession dataTaskWithURL:url completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
         UIImage *image = [UIImage imageWithData:data];
         dispatch_async(dispatch_get_main_queue(), ^(void) {
+            if (self.tag != url.hash) {
+                return;
+            }
             self.backgroundColor = UIColor.clearColor;
             self.image = image;
         });
